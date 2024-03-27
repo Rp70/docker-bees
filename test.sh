9cc742eeeef39a571277459e0873ed6f587842fb
Rp70 <lequocdung99vn@yahoo.com>
2024-03-27 08:04:49 +0000

    refactor: specific log options for daemon instance

--- test.sh
+++ test.sh
@@ -2,9 +2,13 @@
 # THIS IS FOR DEVELOPMENT ONLY.
 
 #set -ex
-docker run -ti --rm --privileged \
+docker run -d --rm --privileged \
     -e HASH_TABLE=/mnt/.beeshome/beeshash.dat \
     -e HASH_TABLE_SIZE=4G \
     -e OPTIONS="-a -c 2" \
-    -v /backup:/mnt \
+    -v /data:/mnt \
+    --log-driver json-file \
+    --log-opt mode=non-blocking \
+    --log-opt max-size=1m \
+    --log-opt max-file=5 \
     rp70/bees $1
